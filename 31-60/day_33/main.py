a023fe873db7e3f2b223f275eb622cd78c86edd7
Anmar <117080591+AnmarX@users.noreply.github.com>
2023-05-15 15:16:12 -0700

    day  33 end

--- 31-60/day_33/main.py
+++ 31-60/day_33/main.py
@@ -4,26 +4,88 @@
 
 import requests
 import json
+from datetime import datetime
+import smtplib
+import time
+
+print("start")
+time.sleep(10)
+print("end")
 
 
 
-def f(fun):
-    def wrpper(x,y):
-        print("1")
-        ss=fun(x,y)
-        print("2")
-        return ss
-    return wrpper
-@f
-def hi(x,y):
-    print("hi")
-    return x+y
-print(hi(4,5))
+# MY_EMAIL = "___YOUR_EMAIL_HERE____"
+# MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
+# MY_LAT = 51.507351 # Your latitude
+# MY_LONG = -0.127758 # Your longitude
 
 
-a=lambda x,y:x+y
+# def is_iss_overhead():
+#     response = requests.get(url="http://api.open-notify.org/iss-now.json")
+#     response.raise_for_status()
+#     data = response.json()
 
-print(a(3,33))
+#     iss_latitude = float(data["iss_position"]["latitude"])
+#     iss_longitude = float(data["iss_position"]["longitude"])
+
+#     #Your position is within +5 or -5 degrees of the iss position.
+#     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+#         return True
+
+
+# def is_night():
+#     parameters = {
+#         "lat": MY_LAT,
+#         "lng": MY_LONG,
+#         "formatted": 0,
+#     }
+#     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+#     response.raise_for_status()
+#     data = response.json()
+#     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+#     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+#     time_now = datetime.now().hour
+
+#     if time_now >= sunset or time_now <= sunrise:
+#         return True
+
+
+# while True:
+#     time.sleep(60)
+#     if is_iss_overhead() and is_night():
+#         connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
+#         connection.starttls()
+#         connection.login(MY_EMAIL, MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs=MY_EMAIL,
+#             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+#         )
+
+
+
+
+
+
+
+
+# def f(fun):
+#     def wrpper(x,y):
+#         print("1")
+#         ss=fun(x,y)
+#         print("2")
+#         return ss
+#     return wrpper
+# @f
+# def hi(x,y):
+#     print("hi")
+#     return x+y
+# print(hi(4,5))
+
+
+# a=lambda x,y:x+y
+# print(a(3,33))
 
 # # #decorator 
 # # #the first function take func as an arrgument 
@@ -61,6 +123,8 @@ print(a(3,33))
 # data = response.json()[0]
 # print(data)
 # data2 = response.json()
+# # #this will generate an error for some reason the endpoint will generate an erorr
+# print(data2)
 # q=json.dumps(data2)
 # print(q)
 
